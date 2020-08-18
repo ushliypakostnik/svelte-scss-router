9571bbb4f33025636c049deb27e35a4b4344853c
Sergei Sergeevich <pendangdewa@gmail.com>
2020-08-18 20:21:20 +0530

    add router

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
-  export let name;
+  import { Route } from "tinro";
+  import Home from "./components/Home.svelte";
+  import About from "./components/About.svelte";
+  import Contacts from "./components/Contacts.svelte";
 </script>
 
 <style lang="scss">
@@ -13,13 +16,6 @@
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
@@ -27,12 +23,20 @@
   }
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-</main>
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/contacts">Contacts</a>
+</nav>
 
+<main>
+  <Route path="/">
+    <Home />
+  </Route>
+  <Route path="/about">
+    <About />
+  </Route>
+  <Route path="/contacts">
+    <Contacts />
+  </Route>
+</main>
